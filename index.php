e5c399f9bf25eef728c2e7a33a32188f83b0605a
marcusvlourenco <marcusvvlourenco@gmail.com>
2019-12-14 11:04:00 -0300

    Criação

--- index.php
+++ index.php
@@ -4,36 +4,7 @@
         <meta charset="UTF-8">
         <title></title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <style type="text/css">
-            .titulo1{
-                color: grey;
-                font-family: sans-serif; 
-                font-size: 26px;
-                font-weight: 900;
-            }      
-            .titulo2{
-                color: #ec971f;
-                font-family: sans-serif; 
-                font-size: 20px;
-                font-weight: 600;
-            }        
-            .paragrafo{     
-                font-family: sans-serif; 
-                font-size: 16px;
-                text-align: justify;
-            }
-            a:link, a:visited {
-                    color: #51616e;
-                    text-decoration: none
-                    }
-            a:hover {
-                    text-decoration: underline; 
-                    color: #e1d819;
-                    }
-            a:active {
-                    text-decoration: none
-                    }
-        </style>
+        
     </head>
     <body>
         <div class="titulo1">
